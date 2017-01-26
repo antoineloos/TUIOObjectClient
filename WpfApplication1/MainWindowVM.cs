f431c1e874f793de8ca490adfac360e0583d3e38
totone69 <antoine.almp@hotmail.fr>
2017-01-26 18:26:51 +0100

    first commit

---
+++ WpfApplication1/MainWindowVM.cs
@@ -0,0 +1,111 @@
+﻿using ObjetTangibleTest;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApplication1
+{
+    public class MainWindowVM : BindableBase
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { SetProperty(ref id, value); }
+        }
+
+        private float angle;
+        public float Angle
+        {
+            get { return angle; }
+            set { SetProperty(ref angle, value); }
+        }
+
+        private float posX;
+        public float PosX
+        {
+            get { return posX; }
+            set { SetProperty(ref posX, value); }
+        }
+
+
+
+        private float posY;
+        public float PosY
+        {
+            get { return posY; }
+            set { SetProperty(ref posY, value); }
+        }
+
+        public MainWindow MainW { get; set; }
+
+        public MainWindowVM(MainWindow mainW)
+        {
+            MainW = mainW;
+            var MainGrid = MainW.mainGr;
+            Thread t = new Thread(() =>
+            {
+                while (true)
+                {
+                    var lst = CltSingleton.Instance.clt.getTuioObjects();
+
+                    if (lst != null && lst.Count > 0)
+                    {
+                        var tmp = lst.FirstOrDefault();
+                        Id = tmp.SymbolID;
+                        System.Windows.Application.Current.Dispatcher.InvokeAsync(new Action(() =>
+                        {
+                            // = tmp.SymbolID;
+                            Canvas.SetLeft(MainGrid, tmp.Position.X * 1920 - 100);
+                            Canvas.SetTop(MainGrid, tmp.Position.Y * 1080 - 100);
+                            MainW.transf.Angle = tmp.AngleDegrees;
+                        }), System.Windows.Threading.DispatcherPriority.Render).Wait();
+
+                        //foreach (TUIO.TuioObject elem in lst)
+                        //{
+
+                        //}
+
+
+
+                    }
+
+
+                }
+            });
+            t.Start();
+
+            //Task.Factory.StartNew(new Action(() =>
+            //{
+
+            //    while (true)
+            //    {
+            //        var lst = CltSingleton.Instance.clt.getTuioObjects();
+
+            //        if (lst != null && lst.Count > 0)
+            //        {
+
+            //            var tmp = lst.FirstOrDefault();
+            //            //foreach (TUIO.TuioObject elem in lst)
+            //            //{
+            //                Id = tmp.SymbolID;
+            //                PosX = tmp.Position.X * 1920-100;
+            //                PosY = tmp.Position.Y * 1080-100;
+            //            Angle = tmp.AngleDegrees;
+            //            //}
+                            
+                        
+
+            //        }
+
+
+            //    }
+            //}));
+        }
+    }
+}
---
+++ WpfApplication1/TObjectViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class TObjectViewModel : BindableBase
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { SetProperty(ref id, value); }
+        }
+
+        private float posX;
+        public float PosX
+        {
+            get { return posX; }
+            set { SetProperty(ref posX, value); }
+        }
+
+
+
+        private float posY;
+        public float PosY
+        {
+            get { return posY; }
+            set { SetProperty(ref posY, value); }
+        }
+        public TObjectViewModel(int id, float x, float y)
+        {
+            Id = id;
+            PosX = x;
+            PosY = y;
+        }
+
+        public void Update(int id, float x, float y)
+        {
+            Id = id;
+            PosX = x;
+            PosY = y;
+        }
+    }
+}
